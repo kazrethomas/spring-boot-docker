791f33c8a4f16477fb3519b93df765a0cb4d69d9
kazrethomas <kazre.thomas@cod-it.tech>
2023-05-11 13:50:31 -0400

    update

--- src/main/java/com/example/dockerize/springbootapp/SpringBootAppApplication.java
+++ src/main/java/com/example/dockerize/springbootapp/SpringBootAppApplication.java
@@ -7,21 +7,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
 @RestController
-@RequestMapping("/api/hello")
+@RequestMapping("kazre/api/hello")
 public class SpringBootAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAppApplication.class, args);
 	}
 
-	@Value("${YOUR_NAME}")
-	private String yourName;
+    @Value("${YOUR_NAME}")
+	 private String yourName;
 
 	@GetMapping()
 	public String getGreeting(){
 		return "Hello " + yourName;
 	}
-
 }
--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-helm template  jason ./jason-spring-chart/ --set image.tag=$1 > k8.yaml
+helm template  jason ./kazre-helm/ --set image.tag=$1 > k8.yaml
